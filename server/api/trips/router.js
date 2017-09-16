e272b96f7e2b8eb1318cf83992a9295397918e8a
Stanislav <ghivan.iv@gmail.com>
2017-09-16 22:35:57 +0300

    Trips router

--- server/api/trips/router.js
+++ server/api/trips/router.js
@@ -5,6 +5,7 @@ const rootDir = path.dirname(require.main.filename);
 const tripsDB = require(path.join(rootDir, 'database', 'tripsDB'));
 const locationsDB = require(path.join(rootDir, 'database', 'locationsDB'));
 const customersDB = require(path.join(rootDir, 'database', 'customersDB'));
+const {getNextId} = require(path.join(rootDir, 'database', 'utils'));
 
 router.get('/', (req, res)=> {
     res.locals.title = 'Trips';
@@ -19,38 +20,32 @@ router.get('/', (req, res)=> {
 });
 
 router.post('/add', (req, res) => {
-    res.locals.title = 'Trips';
-    let name = req.body.name.trim();
-    if (!Array.isArray(req.body.locations)) req.body.locations =[req.body.locations];
-    tripsDB.getAll().then(trips => {
-        res.locals.trips = trips;
-        locationsDB.getAll().then(locations => {
-            res.locals.locations = locations;
-            if (trips.find(trip => trip.name.toLowerCase() === name.toLowerCase())){
-                res.locals.errorMessage = 'Маршрут уже есть в базе данных!';
-                tripsDB.setLocationsNames(trips, locations);
-                res.render('trips');
-                return;
+    let name = req.body.name.trim() || 'Новый маршрут ' + new Date(Date.now()).toLocaleString();
+    let requestedLocations = req.body.locations || [];
+    if (!Array.isArray(requestedLocations)) requestedLocations =[requestedLocations];
+
+    Promise.all([tripsDB.getAll(), locationsDB.getAll()]).then(([trips, locations]) => {
+        if (trips.find(trip => trip.name.toLowerCase() === name.toLowerCase())){
+            tripsDB.setLocationsNames(trips, locations);
+            res.render('trips', {errorMessage: 'Маршрут уже есть в базе данных!', title: 'Trips', trips, locations});
+            return;
+        }
+        trips.push({
+            id: getNextId(trips),
+            name: name,
+            route: {
+                locations: requestedLocations.map(location => parseInt(location)),
+                arrivalDate: req.body.arrivalDate,
+                destinationDate: req.body.destinationDate
             }
-            const maxId = Math.max.apply(null, trips.map(trip => trip.id));
-            const id = (maxId > 0) ? maxId + 1 : 1;
-            trips.push({
-                id: id,
-                name: name,
-                route: {
-                    locations: req.body.locations.map(location => parseInt(location)),
-                    arrivalDate: req.body.arrivalDate,
-                    destinationDate: req.body.destinationDate
-                }
-            });
-            tripsDB.add(trips)
-                .then(() => {
-                    tripsDB.setLocationsNames(trips, locations);
-                    res.render('trips')
-                })
-                .catch(()=> res.render('locations', {errorMessage: 'Ошибка записи в базу!'}));
-        })
-    })
+        });
+        tripsDB.add(trips)
+            .then(() => {
+                tripsDB.setLocationsNames(trips, locations);
+                res.render('trips', {title: 'Trips', trips, locations})
+            })
+            .catch(()=> res.render('trips', {errorMessage: 'Ошибка записи в базу!', trips, locations}));
+    });
 });
 
 router.get('/delete/:id', (req, res) => {
