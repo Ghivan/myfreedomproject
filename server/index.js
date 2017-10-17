c842453a814c056b9db3f468e3d120e5fe2b50d3
Stanislav <ghivan.iv@gmail.com>
2017-10-17 21:45:09 +0300

    Rebuilt client

--- server/index.js
+++ server/index.js
@@ -1,12 +1,5 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const router = require('./api/router');
-
-const app = express();
+const app = require('./app');
 const PORT = 3001;
-app.use(express.static(path.join(__dirname,'static')));
-app.use(router);
 
 app.listen(PORT, () => {
     console.log('Server works on port ' + PORT + '.');
